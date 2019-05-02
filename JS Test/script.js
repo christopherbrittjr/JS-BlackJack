752bfb04fea88c8ff135ac46233ddb9fce1b9d2e
Christopher Britt <christopher.brittjr@outlook.com>
2019-05-02 08:16:33 -0400

    Added comments and Game variables

--- JS Test/script.js
+++ JS Test/script.js
@@ -3,15 +3,27 @@
 // by Christopher Britt
 //
 
+//Card variables
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack',
     'Ten', 'Nine', 'Eight', 'Seven', 'Six',
     'Five', 'Four', 'Three', 'Two'];
 
+//DOM variables
 let textArea = document.getElementById('text-area');
 let newGameButton = document.getElementById('new-game-button');
 let hitButton = document.getElementById('hit-button');
 let stayButton = document.getElementById('stay-button');
+
+//Game variables
+let getStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = [];
 
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
