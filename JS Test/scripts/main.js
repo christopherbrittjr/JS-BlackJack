eba544d3fb4bed329d49db3aab83b040d8a60097
Christopher Britt <christopher.brittjr@outlook.com>
2019-05-02 13:57:44 -0400

    Commit

--- JS Test/scripts/main.js
+++ JS Test/scripts/main.js
@@ -4,10 +4,10 @@
 //
 
 //Card variables
-let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
-let values = ['Ace', 'King', 'Queen', 'Jack',
-    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
-    'Five', 'Four', 'Three', 'Two'];
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'],
+    values = ['Ace', 'King', 'Queen', 'Jack',
+              'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+              'Five', 'Four', 'Three', 'Two'];
 
 //DOM variables
 let textArea = document.getElementById('text-area'),
@@ -20,17 +20,19 @@ let getStarted = false,
     gameOver = false,
     playerWon = false,
     dealerCards = [],
+    hitButton.style.display = 'none';
+    stayButton.style.display = 'none';
     playerCards = [],
     dealerScore = 0,
     playerScore = 0,
     deck = [];
 
-hitButton.style.display = 'none';
-stayButton.style.display = 'none';
+
 showStatus();
 
 newGameButton.addEventListener('click', function () {
-    gameStarted = True;
+   
+    gameStarted = true;
     gameOver = false;
     playerWon = false;
 
