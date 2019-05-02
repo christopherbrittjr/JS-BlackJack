73a868fbf265a9e16497309543e6fc1c12dd0dda
Christopher Britt <christopher.brittjr@outlook.com>
2019-05-02 08:30:18 -0400

    Added function shuffleDeck

--- JS Test/script.js
+++ JS Test/script.js
@@ -27,12 +27,22 @@ let getStarted = false,
 
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
+showStatus();
 
 newGameButton.addEventListener('click', function () {
-    textArea.innerText = 'Started...';
+    gameStarted = True;
+    gameOver = false;
+    playerWon = false;
+
+    deck = createDeck();
+    shuffleDeck(deck);
+    dealerCards = [getNextCard, getNextCard];
+    playerCards = [getNextCard, getNextCard];
+
     newGameButton.style.display = 'none';
     hitButton.style.display = 'inline';
     stayButton.style.display = 'inline';
+    showStatus();
 });
 
 function createDeck() {
@@ -49,22 +59,26 @@ function createDeck() {
     return deck;
 }
 
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let swapIdx = Math.trunc(Math.random() * deck.length);
+        let tmp = deck[swapIdx];
+        deck[swapIdx] = deck[i];
+        deck[i] = tmp;
+    }
+}
+
 function getCardString(card) {
     return card.value + ' of ' + card.suit;
 }
-function getNextCard() {
-    return deck.shift();
+
+function showStatus() {
+    if (!gameStarted) {
+        textArea.innerText = 'Welcome to Blackjack!';
+        return;
+    }
 }
 
-let deck = createDeck();
-
-let playerCards = [getNextCard(), getNextCard()]
-
-console.log("Welcom to Blackjack!");
-
-console.log("You are dealt: ");
-console.log("  " + getCardString(playerCards[0]));
-console.log("  " + getCardString(playerCards[1]));
 
 
 
